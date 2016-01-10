f8b11299d4fe7e1c16761d3070ba2c7c2f913ae7
furenku <furenku@gmail.com>
2016-01-09 20:59:33 -0600

    limpiando codigo clase 1

--- clase01/index.php
+++ clase01/index.php
@@ -1,4 +1,16 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+	</head>
+
+	<body>
+
 <?php
+	// PHP básico.
+	
+	// PHP: Hypertext Preprocessor
+
+
 
 	// Comentario:
 	// Cualquier texto después de '//' es ignorado por PHP.
@@ -8,26 +20,51 @@
 
 	/*
 	Este es un comentario multi-línea
-	Abarca varias líneas
+	Abarca todo el texto comprendido
+	entre los símbolos de comentario
 	*/
 
 
-	// funcion "echo": muestra contenido en la pantalla:
+	// 1. Mostrando contenido en la pantalla:
+	
+	// La función "echo": muestra contenido en la pantalla:
+	
 	echo "Hola";
+	
 	echo 1+1;
 
 
+	/*
+
+	NOTA: Observar la sintaxis.
+		1. El comando 'echo' muestra cualquier información que se ubique a su derecha.
+		2. La palabra "Hola" está escrita entre comillas.
+		3. Cada comando debe concluirse con un ';' (punto y coma)
+
+	*/
 
 
-	// Muchas veces se intercala código PHP con HTML:
+
+	/*
+
+	2. Incorporando PHP a HTML
+
+		Para ubicar la información mostrada con PHP dentro de
+		una estructura semántica y/o dentro de un diseño, es
+		necesario ubicarla dentro de etiquetas HTML.
+		Para esto, hay dos estilos:
+
+	*/
+
+
+
+
+	/*
+	2.1 Intercalando PHP con HTML:
+		Se utilizan las etiquetas <?php y ?> para abrir y cerrar bloques de PHP
+	*/
+
 	?>
-
-	<html>
-	<head>
-		<meta charset="UTF-8">
-	</head>
-	<body>
-
 
 		<h3>Código HTML</h3>
 
@@ -39,30 +76,52 @@
 		</div>
 
 
+	<?php 
+
+	/*
+
+	2.2 Escribiendo etiquetas HTML desde PHP
+	
+		Sin salir de PHP, podemos escribir etiquetas HTML
+		utilizando "strings"
+	*/
+	echo '<div>';
+
+		echo '<p>';
+			echo 'Este texto está dentro de un párrafo HTML';
+		echo '</p>';
+
+		echo '<a href="#">';
+			echo 'Este texto es un link';
+		echo '</a>';
+
+	echo '</div>';	
+	?>
 
 
-		<?php
-
-		// VARIABLES
-
-		// son como buzones, puede cambiar su contenido.
 
 
-		$mi_variable = "Cualquier contenido";
-		echo $mi_variable;
-		echo '<br/>';
+	<?php
 
-		$mi_variable = 17;
-		echo $mi_variable;
-		echo '<br/>';
-		
-		$mi_variable = date('D-d-M-Y');
-		echo $mi_variable;
-		echo '<br/>';
-		
-		?>
+	// 3. VARIABLES
+	// cualquier palabra antecedida por un signo de dólar ($) 
+	// se vuelve una variable, a la que podemos asignar cualquier contenido.
+	
+	$mi_variable = "Cualquier contenido"; // un texto
+	echo $mi_variable;
+	echo '<br/>'; // line break para mostrar variable en una sóla línea.
+
+	$mi_variable = 17; // un número
+	echo $mi_variable;
+	echo '<br/>'; // line break para mostrar variable en una sóla línea.
+	
+	$mi_variable = date('D-d-M-Y'); // un objeto que arroja la fecha de hoy en un formato legible.
+	echo $mi_variable;
+	echo '<br/>'; // line break para mostrar variable en una sóla línea.
+	
+	?>
 
 
 
 	</body>
-	</html>
+</html>
